158705b98cd1b013a8c033bde848526d5dede0d8
Ayrton Vitor <ayrtonvitor@pm.me>
2022-10-24 23:12:19 -0300

    fix mds analysis

--- Multivariate Analysis/Multidimensional Scaling/cars.R
+++ Multivariate Analysis/Multidimensional Scaling/cars.R
@@ -6,46 +6,57 @@ library(smacof)
 library(tidyverse)
 library(vegan)
 
+#transform data
 carros <- read_excel("./Downloads/carros.xls")
 View(carros)
 summary(carros)
+head(carros)
+d_carros <- dist(scale(carros[, -c(1, 2)]))
+d_carros_np <- dist(carros[, -c(1, 2)])
 
-d_carros <- dist(carros[, -c(1, 2)])
-
-for (i in 1:4) {
-  print(mds(d_carros, ndim = i)$stress)
+#check if 2 dimensions is good enough for mds
+pk <- function(eig) {
+  x <- sum(abs(eig))
+  return(cumsum(eig) / x)
 }
+print(pk(cmdscale(d_carros, k = 3, eig = TRUE)$eig)[1:4])
 
-MDS <- mds(d_carros, ndim = 2)
-MDS$conf
+#perform mds
+mds <- cmdscale(d_carros, k = 2)
+mds[,1:2]
 
-coord_carros <- MDS$conf %>% as_tibble()
-ggplot(coord_carros, aes(x = D1, y = D2)) +
+coord_carros <- mds[,1:2] %>% as_tibble()
+ggplot(coord_carros, aes(x = V1, y = V2)) +
   geom_point() +
   geom_label_repel(aes(label = carros$Nome)) +
   labs(x = "Dimensão 1", y = "Dimensão 2")
 
+#chose clusters and plot it with data points
 NbClust(data = coord_carros, min.nc = 2, max.nc = 10,
         index = 'all', method = 'ward.D')
 coord_carros$clusters <- kmeans(coord_carros, centers = 2,
                                 nstart = 100)$cluster %>% as.factor()
 
-ggscatter(coord_carros, x = "D1", y = "D2",
+ggscatter(coord_carros, x = "V1", y = "V2",
           label = carros$Nome,
           color = "clusters",
           ellipse = TRUE,
           ggtheme = theme_gray())
 
-pc <- prcomp(carros[, -c(1, 2)], center = TRUE, scale. = TRUE)
+#principal components for biplot
+pc <- prcomp(scale(carros[, -c(1, 2)]))
 pc
 summary(pc)
-(bp <- ggbiplot(pc))
 
-proc <- procrustes(as.matrix(MDS$conf), as.matrix(bp$data))
+pca <- princomp(scale(carros[, -c(1, 2)]))
+biplot(pca)
+
+#procrustes analysis of mds vs biplot
+proc <- procrustes(mds[,1:2], pca$scores[, 1:2])
 proc
 summary(proc)
 
-plot(proc, kind = 2)
+plot(proc)
 residuals(proc)
 
-protest(as.matrix(MDS$conf), as.matrix(bp$data))
+protest(mds[,1:2], pca$scores[,1:2])
